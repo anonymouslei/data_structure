becc681e19916e9af823437e93ab0edda6e9c5a8
Lei Ge <gracegl2014@gmail.com>
2020-09-27 19:28:08 +0200

    update 02-线性结构2.
    不知道为什么操作符重载在win电脑和PTA上不能使用。我猜可能是编译器不同。所以重新写了mul和add函数。在PTA上测试通过

--- 2/02-线性结构2 一元多项式的乘法与加法运算.cpp
+++ 2/02-线性结构2 一元多项式的乘法与加法运算.cpp
@@ -16,6 +16,15 @@ public:
         head_ = new PolyNode;
         head_->Next = NULL;
     }
+    Polynomial & operator = (const Polynomial & p1)
+    {
+        head_ = p1.head_;
+        return * this;
+    }
+    Polynomial(Polynomial & P) {
+        head_ = P.head_;
+        head_->Next = P.head_->Next;
+    }
 
     ~Polynomial()
     {
@@ -89,7 +98,7 @@ public:
                 while (rear->Next && rear->Next->expon > e) //持续判断，直到rear到达第一个比e小或者等于e的地址
                     rear = rear->Next;
                 if (rear->Next && rear->Next->expon == e) { //如果系数相等
-                    if (rear->Next->coef)
+                    if (rear->Next->coef + c)
                         rear->Next->coef += c;
                     else { // 计算结果等于零，就把这一项删掉
                         t = rear->Next;
@@ -115,7 +124,7 @@ public:
         return P;
     }
 
-    Polynomial operator+(const Polynomial & P2)
+    Polynomial & operator+(const Polynomial & P2)
     {
         Polynomial P;
         PolynomialNode t1, t2, rear, tmp;
@@ -123,7 +132,7 @@ public:
 
         t1 = head_;
         t2 = P2.head_;
-        P = Polynomial();
+//        P = Polynomial();
         rear = P.head_;
         while (t1 && t2) {
             if (t1->expon == t2->expon) {
@@ -156,6 +165,101 @@ public:
 
         return P;
     }
+
+    void Add(const Polynomial & P2, Polynomial & P)
+    {
+//        Polynomial P;
+        PolynomialNode t1, t2, rear, tmp;
+        int sum;
+
+        t1 = head_;
+        t2 = P2.head_;
+//        P = Polynomial();
+        rear = P.head_;
+        while (t1 && t2) {
+            if (t1->expon == t2->expon) {
+                sum = t1->coef + t2->coef;
+                if (sum)
+                    Attach(sum, t1->expon, rear);
+                t1 = t1->Next;
+                t2 = t2->Next;
+            } else if (t1->expon > t2 ->expon) {
+                Attach(t1->coef, t1->expon, rear);
+                t1 = t1->Next;
+
+            } else {
+                Attach(t2->coef, t2->expon, rear);
+                t2 = t2->Next;
+
+            }
+        }
+        while (t1) {
+            Attach(t1->coef, t1->expon, rear);
+            t1 = t1->Next;
+
+        }
+        while (t2) {
+            Attach(t2->coef, t2->expon, rear);
+            t2 = t2->Next;
+
+        }
+        rear ->Next = NULL;
+
+        tmp = P.head_;
+        P.head_ = P.head_->Next;
+        delete tmp;
+    }
+
+    void Mul(const Polynomial & P2, Polynomial & P) {
+//        Polynomial P;
+        PolynomialNode t1, t2, rear, t;
+        int c, e;
+        if (!head_ || !P2.head_)
+            return ;
+
+        t1 = head_;
+        t2 = P2.head_;
+        rear = P.head_;
+        while (t2)  // 先用P1的第一项乘以P2, 得到P
+        {
+            Attach(t1->coef*t2->coef, t1->expon+t2->expon, rear);
+            t2 = t2->Next;
+        }
+        t1 = t1->Next;
+        while (t1)
+        {
+            t2 = P2.head_;
+            rear = P.head_;
+            while (t2) {
+                e = t1->expon + t2->expon;
+                c = t1->coef * t2->coef;
+                while (rear->Next && rear->Next->expon > e) //持续判断，直到rear到达第一个比e小或者等于e的地址
+                    rear = rear->Next;
+                if (rear->Next && rear->Next->expon == e) { //如果系数相等
+                    if (rear->Next->coef + c)
+                        rear->Next->coef += c;
+                    else { // 计算结果等于零，就把这一项删掉
+                        t = rear->Next;
+                        rear->Next = t->Next;
+                        delete t;
+                    }
+                } else { // 小于，就申请新的节点，进行赋值
+                    t = new PolyNode;
+                    t->coef = c;
+                    t->expon = e;
+                    t->Next = rear->Next;
+                    rear->Next = t;
+                    rear = rear->Next;
+                }
+                t2 = t2->Next;
+            }
+            t1 = t1->Next;
+        }
+
+        t2 = P.head_;
+        P.head_ = P.head_->Next;
+        delete t2;
+    }
     void PrintPoly() {
         int flag = 0;
         PolynomialNode tmp = head_;
@@ -166,11 +270,10 @@ public:
         }
         while (tmp) {
             if (!flag) flag = 1;
-            else {
+            else
                 cout << " ";
-                cout << tmp->coef << " " << tmp->expon;
-                tmp = tmp->Next;
-            }
+            cout << tmp->coef << " " << tmp->expon;
+            tmp = tmp->Next;
         }
         cout << endl;
     }
@@ -182,10 +285,20 @@ private:
 
 int main() {
     Polynomial P1, P2;
-    P1.ReadPoly();
-    P2.ReadPoly();
-    Polynomial P3 = P1 + P2;
-    Polynomial P4 = P1 * P2;
+    int input1[5] = {2, 2, 1, 1, 0};
+    int input2[5] = {2, -2, 1, -1, 0};
+//    int input1[9] = {4, 3, 4, -5, 2, 6, 1, -2, 0};
+//    int input2[7] = {3, 5, 20, -7, 4, 3, 1};
+
+    P1.ReadPoly(input1);
+    P2.ReadPoly(input2);
+//    Polynomial P5 = P2;
+    Polynomial P3;
+    Polynomial P4;
+    P1.Add(P2, P3);
+//    Polynomial P3 = P1 + P2;
+//    Polynomial P4 = P1 * P2;
+    P1.Mul(P2, P4);
     P4.PrintPoly();
     P3.PrintPoly();
     return 0;
